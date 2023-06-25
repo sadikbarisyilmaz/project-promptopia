dae5123352284fec2b20493cbdb6ace7bc5f3a9d
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-06-25 21:58:40 +0300

    my profile added

---
+++ app/profile/page.jsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalised profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,15 +1,18 @@
 "use client";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
-
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
   const [copied, setCopied] = useState("");
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => setCopied(""), 3000);
   };
-  console.log(post);
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -50,6 +53,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === `/profile` && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            onClick={handleEdit}
+            className="font-inter text-sm green_gradient cursor-pointer"
+          >
+            Edit
+          </p>
+          <p
+            onClick={handleDelete}
+            className="font-inter text-sm orange_gradient cursor-pointer"
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
